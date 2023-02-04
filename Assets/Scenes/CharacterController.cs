a5fd1e326b5b37e1a965fd06dc828e77b6831402
Ben Brun <derstanby@gmail.com>
2023-02-04 18:26:12 +0100

    [ADD] Character 2D Collider

--- Assets/Scenes/CharacterController.cs
+++ Assets/Scenes/CharacterController.cs
@@ -2,15 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour
+public class Controller : MonoBehaviour
 {
     public Animator animator;
     public float speed = 1.0f;
-    public float jumpSpeed = 20.0f;
-    // public float distToGround = 10.0f;
-    // public LayerMask groundLayer;
-    // public bool doubleJump = true;
-    // public bool canjump_double = true;
+    public float jumpSpeed = 1.0f;
+    public float distToGround = 10.0f;
+    public LayerMask groundLayer;
+    public bool doubleJump = true;
+    public bool canjump_double = true;
 
     public void Hit()
     {
@@ -22,15 +22,15 @@ public class CharacterController : MonoBehaviour
         animator.SetBool("hit", false);
     }
 
-    /* bool IsGrounded()
+    bool IsGrounded()
     {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector3.up, distToGround + (float)0.1, groundLayer);
         return (hit.collider != null);
-    } */
+    }
 
-    void Update()
+    public void Update()
     {
-        // bool grounded = false;
+        bool grounded = false;
         if (Input.GetKey("q"))
         {
             animator.SetBool("walk", true);
@@ -42,15 +42,13 @@ public class CharacterController : MonoBehaviour
             transform.Translate(Vector3.right * speed * Time.deltaTime);
         } else
         {
-            animator.SetBool("walk", false);
-            animator.SetBool("run", false);
+            animator.SetBool("walk", true);
+            animator.SetBool("run", true);
         }
 
         if (Input.GetKey("space"))
         {
-            animator.SetBool("jump", true);
-            GetComponent<Rigidbody2D>().AddForce(Vector3.up * jumpSpeed * Time.deltaTime);
-            /*grounded = IsGrounded();
+            grounded = IsGrounded();
             if (grounded || doubleJump)
             {
                 if (!grounded && doubleJump)
@@ -58,22 +56,14 @@ public class CharacterController : MonoBehaviour
                     doubleJump = false;
                 }
                 GetComponent<Rigidbody2D>().AddForce(Vector3.up * jumpSpeed);
-            }*/
-        } else
-        {
-            animator.SetBool("jump", false);
+            }
         }
-
-        if (Input.GetButtonDown("Fire1"))
-        {
-            animator.SetBool("attack", true);
-        }
-        /*if (canjump_double && !doubleJump)
+        if (canjump_double && !doubleJump)
         {
             if (IsGrounded())
             {
                 doubleJump = true;
             }
-        }*/
+        }
     }
 }
